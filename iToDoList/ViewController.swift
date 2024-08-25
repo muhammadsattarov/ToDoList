eccb86f13b8885239dcccde4474f4aca65a8df19
Muhammad Sattarov <sattarovmuhammad98@gmail.com>
2024-08-25 16:02:08 +0500

    Initial Commit

---
+++ iToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iToDoList
+//
+//  Created by user on 25/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
